e515a873367c2c24e920af80874d5808ffd9d2a6
abrilcapizzano <103966017+abrilcapizzano@users.noreply.github.com>
2023-03-30 23:16:19 -0300

    test1

--- src/componentes/contact/Contact.jsx
+++ src/componentes/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./contact.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import wpp from "./WhatsApp.svg";
+import wpp from "./whatsappicon.svg";
 import { useForm, ValidationError } from '@formspree/react';
 function Contacto() {
     const [state, handleSubmit] = useForm("mjvdozwr");
@@ -10,12 +10,13 @@ function Contacto() {
     }
   return (
     <div className="contacto">
-        <h2>Contact me by WhatsApp or email</h2>
+        <h2>Contact me by WhatsApp</h2>
         <a href="https://wa.me/541124682268/" target="_blank" rel="noreferrer">
         <img src={wpp} className="icon" alt="Whatsapp Logo" />
  </a>
 
  <form onSubmit={handleSubmit}>
+ <h2>Or Email</h2>
   <label className="label-input">Type your Email</label>
       <input
         id="email"
